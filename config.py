8fe1e9b1e54eaf26eb4ff6714a41d4f6d2e128db
Luck Mix <153753933+lk3140318@users.noreply.github.com>
2024-12-23 15:58:57 +0530

    Update config.py

--- config.py
+++ config.py
@@ -18,7 +18,7 @@ def is_enabled(value, default):
         return default
       
 # Bot Information
-API_ID = int(environ.get("API_ID", ""24335028))
+API_ID = int(environ.get("API_ID", "24335028"))
 API_HASH = environ.get("API_HASH", "b204ec833fb451fb913fc8e683b232d0")
 BOT_TOKEN = environ.get("BOT_TOKEN", "")
 
