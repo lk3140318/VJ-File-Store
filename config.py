8504cbb460d25efcd455509d0bf6f5f5a3c3dd6a
Luck Mix <153753933+lk3140318@users.noreply.github.com>
2025-01-26 18:25:30 +0530

    Update config.py

--- config.py
+++ config.py
@@ -28,15 +28,15 @@ BOT_USERNAME = environ.get("BOT_USERNAME", "") # without @
 PORT = environ.get("PORT", "8080")
 
 # Clone Info :-
-CLONE_MODE = bool(environ.get('CLONE_MODE', False)) # Set True or False
+CLONE_MODE = bool(environ.get('CLONE_MODE', True)) # Set True or False
 
 # If Clone Mode Is True Then Fill All Required Variable, If False Then Don't Fill.
-CLONE_DB_URI = environ.get("CLONE_DB_URI", "")
-CDB_NAME = environ.get("CDB_NAME", "clonetechvj")
+CLONE_DB_URI = environ.get("CLONE_DB_URI", "mongodb+srv://yexecax88:dUpjO0Z7esWfXESJ@cluster0.saf78.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+CDB_NAME = environ.get("CDB_NAME", "yexecax88")
 
 # Database Information
-DB_URI = environ.get("DB_URI", "")
-DB_NAME = environ.get("DB_NAME", "techvjbotz")
+DB_URI = environ.get("DB_URI", "mongodb+srv://hopepaw650:q9lpAOJZCnKTrDE8@clustepiro.7q6ji.mongodb.net/?retryWrites=true&w=majority&appName=Clustepiro")
+DB_NAME = environ.get("DB_NAME", "hopepaw650")
 
 # Auto Delete Information
 AUTO_DELETE_MODE = bool(environ.get('AUTO_DELETE_MODE', True)) # Set True or False
