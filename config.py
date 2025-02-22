16292c8e471c96ff7e77896fe50ec0191bc14b7d
Luck Mix <153753933+lk3140318@users.noreply.github.com>
2025-02-23 01:41:33 +0530

    Update config.py

--- config.py
+++ config.py
@@ -24,7 +24,7 @@ BOT_TOKEN = environ.get("BOT_TOKEN", "")
 
 PICS = (environ.get('PICS', 'https://graph.org/file/ce1723991756e48c35aa1.jpg')).split() # Bot Start Picture
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '').split()]
-BOT_USERNAME = environ.get("BOT_USERNAME", "") # without @
+BOT_USERNAME = environ.get("BOT_USERNAME", "File_sharing_A_2_Z_Bot") # without @
 PORT = environ.get("PORT", "8080")
 
 # Clone Info :-
